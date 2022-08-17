8c64706ce0d1c9e51c4e30a86ea8b97af10601d0
Anderson Silva <andpita47@gmail.com>
2022-08-17 19:46:44 -0300

    tentativa de correção localhost

--- src/server.js
+++ src/server.js
@@ -1,4 +1,7 @@
 import app from './app';
 
 const port = process.env.APP_PORT;
-app.listen(port);
+app.listen(port, () => {
+  console.log(`Acessar http://localhost:${port}`);
+  console.log('Servidor executando na porta 3001');
+});

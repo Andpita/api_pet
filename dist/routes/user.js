bbb5b96c882cf2535008c2a00b735b20aeac4b3c
Anderson Silva <andpita47@gmail.com>
2022-09-24 19:42:49 -0300

    update de users 3. adicionando loginRequired

--- dist/routes/user.js
+++ dist/routes/user.js
@@ -1,7 +1,7 @@
 "use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _express = require('express');
 var _User = require('../controllers/User'); var _User2 = _interopRequireDefault(_User);
 
-//import loginRequired from '../middlewares/loginRequired';
+var _loginRequired = require('../middlewares/loginRequired'); var _loginRequired2 = _interopRequireDefault(_loginRequired);
 
 const router = new (0, _express.Router)();
 
@@ -10,7 +10,7 @@ router.get('/', _User2.default.index); //< Mostra todos usuários
 router.get('/:id', _User2.default.show); //< Mostra apenas um usuaŕio logado, não um especfico
 
 router.post('/', _User2.default.store);
-router.put('/:id', _User2.default.update);
-router.delete('/:id', _User2.default.delete);
+router.put('/:id', _loginRequired2.default, _User2.default.update);
+router.delete('/:id', _loginRequired2.default, _User2.default.delete);
 
 exports. default = router;
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import user from '../controllers/User';
 
-//import loginRequired from '../middlewares/loginRequired';
+import loginRequired from '../middlewares/loginRequired';
 
 const router = new Router();
 
@@ -10,7 +10,7 @@ router.get('/', user.index); //< Mostra todos usuários
 router.get('/:id', user.show); //< Mostra apenas um usuaŕio logado, não um especfico
 
 router.post('/', user.store);
-router.put('/:id', user.update);
-router.delete('/:id', user.delete);
+router.put('/:id', loginRequired, user.update);
+router.delete('/:id', loginRequired, user.delete);
 
 export default router;

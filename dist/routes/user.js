816f96d6a64a1050835d5c4c5f955343560905e6
Anderson Silva <andpita47@gmail.com>
2022-09-23 21:50:18 -0300

    alteracao rotas users

--- dist/routes/user.js
+++ dist/routes/user.js
@@ -9,8 +9,8 @@ const router = new (0, _express.Router)();
 router.get('/', _User2.default.index); //< Mostra todos usuários
 // router.get('/:id', user.show); //< Mostra apenas um usuaŕio logado, não um especfico
 
-router.post('/', _User2.default.store);
-router.put('/', _loginRequired2.default, _User2.default.update);
-router.delete('/', _loginRequired2.default, _User2.default.delete);
+router.post('/:id', _User2.default.store);
+router.put('/:id', _loginRequired2.default, _User2.default.update);
+router.delete('/:id', _loginRequired2.default, _User2.default.delete);
 
 exports. default = router;
--- src/routes/user.js
+++ src/routes/user.js
@@ -9,8 +9,8 @@ const router = new Router();
 router.get('/', user.index); //< Mostra todos usuários
 // router.get('/:id', user.show); //< Mostra apenas um usuaŕio logado, não um especfico
 
-router.post('/', user.store);
-router.put('/', loginRequired, user.update);
-router.delete('/', loginRequired, user.delete);
+router.post('/:id', user.store);
+router.put('/:id', loginRequired, user.update);
+router.delete('/:id', loginRequired, user.delete);
 
 export default router;

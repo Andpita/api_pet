c3e4933603e21656b2e257bb972405c5f864a925
Anderson 'Pita' Silva <dev.andpita@gmail.com>
2024-03-18 05:57:57 -0300

    update package

--- src/server.js
+++ src/server.js
@@ -3,5 +3,5 @@ import app from './app';
 const port = process.env.APP_PORT;
 app.listen(port, () => {
   console.log(`Acessar http://localhost:${port}`);
-  console.log('Servidor executando na porta 3001');
+  console.log(`Servidor executando na porta ${port}`);
 });
